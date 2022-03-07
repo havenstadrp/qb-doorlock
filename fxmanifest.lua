0de3b0e71f66cd5287bf92bf970b896dd42f4228
bedar89 <bedar89@gmail.com>
2022-03-07 20:35:18 +0000

    changed to nl locale

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -10,7 +10,7 @@ shared_scripts {
     'config.lua',
     'configs/*.lua',
     '@qb-core/shared/locale.lua',
-    'locales/en.lua' -- Change this to your preferred language
+    'locales/nl.lua' -- Change this to your preferred language
 }
 
 server_script 'server/main.lua'
